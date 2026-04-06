721cca83ae25e3d24b371e61f3abf205cc0077e4
Rakshita Singh <rakshitasingh357@gmail.com>
2026-04-06 21:44:19 +0530

    Update and rename save_model.py to load_model.py

---
+++ notebook/week_4/load_model.py
@@ -0,0 +1,10 @@
+import pickle
+
+def load_model():
+    with open("model.pkl", "rb") as f:
+        model = pickle.load(f)
+
+    with open("vectorizer.pkl", "rb") as f:
+        vectorizer = pickle.load(f)
+
+    return model, vectorizer
